005f31170f48ddb82f6b7c68311217544a55e8b9
andres <andres@senac.es>
2012-02-27 13:02:19 +0000

    Sample application.

---
+++ metasim/metasim/examples/example1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <metasim/core/adapted.hpp>
+#include <metasim/core/reflective.hpp>
+#include <metasim/gui/ReflectiveGUI.hpp>
+#include <metasim/core/reflective.ipp>
+
+struct MyStruct
+{
+    int a;
+    std::string str;
+};
+
+BOOST_FUSION_ADAPT_STRUCT(MyStruct, (int, a) (std::string, str))
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+
+    QWidget * w = metasim::gui::createWidget(metasim::core::reflective< MyStruct >::get_instance(), NULL);
+    w->show();
+    
+    return app.exec();
+} 
