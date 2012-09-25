e4850d75499e0a3dae853917b224792dc8ea17c1
andres@senac.es <andres@senac.es@6a887da5-8e5e-6d2e-f52f-3bb2b046c35a>
2012-09-25 07:34:52 +0000

    Version 0.2.1

---
+++ tags/0.2.1/corbasim_app/app_servant.hpp
@@ -0,0 +1,21 @@
+#ifndef APP_SERVANT_HPP
+#define APP_SERVANT_HPP
+
+#include "appS.h"
+#include "app_adapted.hpp"
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // APP_SERVANT_HPP
+
+
---
+++ tags/0.2.1/corbasim_idl/share/corbasim/cmake/CorbasimEnv.cmake
@@ -0,0 +1,37 @@
+
+set(CORBASIM_INSTALL_PREFIX /tmp/prueba_corbasim2)
+
+include_directories(${CORBASIM_INSTALL_PREFIX}/include)
+link_directories(${CORBASIM_INSTALL_PREFIX}/lib)
+
+# Boost C++ Libraries
+include_directories(/usr/local/include)
+link_directories(/usr/lib)
+
+# ORB
+set(CORBASIM_ORBIMPL
+    TAO)
+
+set(CORBASIM_ORB_LIBS
+    ACE;TAO;TAO_PortableServer;TAO_AnyTypeCode;TAO_CosNaming;TAO_ObjRefTemplate)
+
+include_directories(/usr/local/ACE+TAO-5.7.9/include;/usr/local/ACE+TAO-5.7.9/include;/usr/local/ACE+TAO-5.7.9/include/orbsvcs)
+link_directories(/usr/local/ACE+TAO-5.7.9/lib)
+
+set(CORBASIM_ORB_IDL_COMPILER
+    tao_idl)
+
+set(CORBASIM_ORB_IDL_COMPILER_OPTIONS
+    )
+
+# Libraries for clients
+set(CORBASIM_CLIENT_LIBS 
+    corbasim corbasim_qt corbasim_gui boost_program_options)
+
+set(CORBASIM_CLIENT_STATIC_LIBS 
+    corbasim_gui_s corbasim_qt_s corbasim_s boost_program_options)
+
+# Libraries for servers
+set(CORBASIM_SERVER_LIBS 
+    corbasim boost_program_options)
+
