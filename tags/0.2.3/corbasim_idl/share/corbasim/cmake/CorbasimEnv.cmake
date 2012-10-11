fa12bc72bfca68fbbd20601d3719fa47cf32298e
andres@senac.es <andres@senac.es@6a887da5-8e5e-6d2e-f52f-3bb2b046c35a>
2012-10-11 11:25:24 +0000

    New revision

---
+++ tags/0.2.3/corbasim_idl/share/corbasim/cmake/CorbasimEnv.cmake
@@ -0,0 +1,37 @@
+
+set(CORBASIM_INSTALL_PREFIX /usr/local)
+
+include_directories(${CORBASIM_INSTALL_PREFIX}/include)
+link_directories(${CORBASIM_INSTALL_PREFIX}/lib)
+
+# Boost C++ Libraries
+include_directories(/usr/include)
+link_directories(/usr/lib)
+
+# ORB
+set(CORBASIM_ORBIMPL
+    TAO)
+
+set(CORBASIM_ORB_LIBS
+    ACE;TAO;TAO_PortableServer;TAO_AnyTypeCode;TAO_CosNaming;TAO_ObjRefTemplate)
+
+include_directories(/usr/include;/usr/include;/usr/include/orbsvcs)
+link_directories(/usr/lib)
+
+set(CORBASIM_ORB_IDL_COMPILER
+    tao_idl)
+
+set(CORBASIM_ORB_IDL_COMPILER_OPTIONS
+    )
+
+# Libraries for clients
+set(CORBASIM_CLIENT_LIBS 
+    corbasim corbasim_qt corbasim_gui boost_program_options)
+
+set(CORBASIM_CLIENT_STATIC_LIBS 
+    corbasim_gui_s corbasim_qt_s corbasim_s boost_program_options)
+
+# Libraries for servers
+set(CORBASIM_SERVER_LIBS 
+    corbasim boost_program_options)
+
